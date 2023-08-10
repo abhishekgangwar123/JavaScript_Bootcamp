d8932d64140a417303c357ce671f749be3ebf0c8
Abhishek Gangwar <114344862+abhishekgangwar123@users.noreply.github.com>
2023-08-10 23:26:33 +0530

    Add files via upload

---
+++ Variables/script.js
@@ -0,0 +1,17 @@
+// var a = "abhishek";
+// var b = 23;
+// {
+//     var a = 12;
+// }
+// console.log(a)
+
+let a = "mango";
+let b = 45;
+{
+    let a = 12;
+    console.log(a);
+}
+console.log(a);
+
+
+// const q = "12";   ------it will not be changed
