135b57e0ae5458cde20cf9ad841cc36dac85d75c
Abhishek Gangwar <114344862+abhishekgangwar123@users.noreply.github.com>
2023-08-10 23:45:59 +0530

    Add files via upload

---
+++ Primitives and Objects/script.js
@@ -0,0 +1,23 @@
+// Primitives in JS
+let a = null;
+let b = true;
+let c = undefined;
+let d = BigInt(234);
+let e = 234;
+let f = "abhishek";
+let g = Symbol('this is a symbol');
+
+console.log(a, b, c, d, e, f, g);
+
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof g);
+
+// Objects in JS
+
+const item = {
+    name: "Abhisek",
+    age: 20,
+    color: blue
+}
+console.log(item[name]);
