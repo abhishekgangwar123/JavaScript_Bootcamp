a2949c0815561a25a36c9f406d3ef48ecb918b00
Abhishek Gangwar <114344862+abhishekgangwar123@users.noreply.github.com>
2025-12-09 15:29:30 +0530

    Add files via upload

---
+++ Operators/script.js
@@ -0,0 +1,55 @@
+//Arithmetic operators-------------------------------------
+
+// let a = 10;
+// let b = 5;
+
+// console.log("a = ", a , "b = ", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+
+
+//Modulus Operator
+// console.log("a % b = ", a % b);
+
+//Exponentiation Opertor 
+// console.log("a ** b = ", a ** b);
+
+//Increment and Decrement are Unary Operator
+// a++;
+// b--; 
+// console.log("a = ", a);
+// console.log("b =  ",b);
+
+//Assignment Operator ( = , += , -= , *= , %= , **= )--------
+
+// let a = 10;
+// let b = 5;
+
+// a += 4;
+// console.log("a = ", a);
+
+// b -= 2;
+// console.log("b = ", b);
+
+//Comparasion Operator[ > , < , >= , <= , ==(Equal to) , ===(Equal to and type) , !=(not equal to) , !==(Not equal to and type) ]
+
+// let a = 10;
+// let b = "10";
+
+// console.log("a == b ", a ==b );
+// console.log("a != b ", a !=b );
+// console.log("a == b ", a ==b );
+
+// console.log("a === b ", a ===b );
+
+//LOgical Operators ( &&(logical AND) , ||(logical OR) , !(logical NOT) ) ------------------------------
+
+let a = 10;
+let b = 5;
+
+let cond1 = a > b;
+let cond2 = a === 6;
+
+console.log("cond1 && cond2 = ", cond1 && cond2);
