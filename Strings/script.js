7dfa1f7a238100593aac8fd6796a5c7ebcf8f85d
Abhishek Gangwar <114344862+abhishekgangwar123@users.noreply.github.com>
2025-12-10 17:14:06 +0530

    Add files via upload

---
+++ Strings/script.js
@@ -0,0 +1,63 @@
+// Strings are the sequences of characters used to represent text.
+
+// let str = "Abhishek";
+// let str2 = 'gangwar';
+
+// console.log(str[1]);
+
+//---------------------------------------------
+
+//Temperate Literals
+
+// let sentence = `This is a special string`;
+
+// console.log(typeof sentence);
+
+//---------------------------------------------
+
+// let obj = {
+//     item : "Pen",
+//     price : 10,
+// };
+
+// console.log("The cost of", obj.item , "is", obj.price , "rupees");
+
+// let output = `The cost of ${obj.item} is \n${obj.price} rupees`;
+// console.log(output);
+
+//---------------------------------------------
+
+// let str = "abhishek";
+// let newStr = str.toUpperCase();
+
+// console.log(str);
+// console.log(newStr);
+
+//----------------------------------------------
+
+// let str = "      Abhishek Gangwar     ";
+// let newStr = str.trim();
+
+// console.log(newStr);
+
+//----------------------------------------------
+
+// let str = "0123456789";
+
+// console.log(str.slice(1, 4)); 
+
+//---------------------------------------------
+
+// let str1 = "Abhishek";
+// let str2 = "Gangwar";
+
+// console.log(str1.concat( str2));
+
+//---------------------------------------------
+
+let str = "hello";
+
+console.log(str.replace("h","y"));
+
+console.log(str.charAt(0));
+
